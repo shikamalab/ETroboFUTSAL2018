329039a63502c7b90603e10b3edd66c232a792da
zawasann <40816977+zawasann@users.noreply.github.com>
2018-09-02 15:33:57 +0900

    Add files via upload

---
+++ Scenario.h
@@ -0,0 +1,35 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef SCENARIO_H_
+#define SCENARIO_H_
+
+#include "ev3api.h"
+//#include "RunMethod.h"
+#include "RunPattern.h"
+#include "Block.h"
+
+class Scenario
+{
+private:
+	RunPattern* mRP;
+	RoboLoc* mRloc;
+	Block_area*     mBArea;
+	int block_ptn;
+public:
+	Scenario(RunPattern*, RoboLoc*, Block_area*);
+	void R(void);
+	void L(void);
+	void RGB2HSV(void);
+	void TEST(void);
+	void DoPuzzle(void);
+	int Block(void);
+	void L_SHORT(void);
+};
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
